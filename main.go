85a1b6f2333f9654634dcd53c3715b29bc484abd
Maria Jimenez Puerta <1609442@tcs.com>
2023-04-12 19:47:06 -0500

    Siguiente commit

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 //importaciones
 import (
+	"fmt"
+
 	"github.com/ptilotta/godesde0/variables"
 )
 
 // funcion, declaracion de variables, resto de codigo etc..
 func main() {
-	variables.MuestroEnteros()
+	Estado, texto := variables.ConviertoaTexto(1588)
+
+	fmt.Println(Estado)
+	fmt.Println(texto)
 }
